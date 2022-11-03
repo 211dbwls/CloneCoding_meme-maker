723164641c414eaee56c87d3ce3924ab03368628
211dbwls <211dbwls@naver.com>
2022-11-03 17:49:47 +0900

    [meme-maker] 드래그해서 선 그리기

---
+++ js/app.js
@@ -0,0 +1,30 @@
+const canvas = document.querySelector("canvas")
+const ctx = canvas.getContext("2d")
+
+canvas.width = 800;
+canvas.height = 800;
+
+ctx.lineWidth = 2;
+
+let isPainting = false;
+function onMove(event) {
+    if(isPainting) {
+        ctx.lineTo(event.offsetX, event.offsetY);
+        ctx.stroke();
+        return;
+    }
+    ctx.moveTo(event.offsetX, event.offsetY);
+}
+
+function startPaintint() {
+    isPainting = true;
+}
+
+function cancelPainting() {
+    isPainting = false;
+}
+
+canvas.addEventListener("mousemove", onMove);
+canvas.addEventListener("mousedown", startPaintint);
+canvas.addEventListener("mouseup", cancelPainting);
+canvas.addEventListener("mouseleave", cancelPainting);
