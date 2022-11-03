8342b4ddc09ff93dfa6b123223df7d2ff8de9b44
211dbwls <211dbwls@naver.com>
2022-11-03 19:05:21 +0900

    [meme-maker] 채우기, 지우기, 리셋

--- js/app.js
+++ js/app.js
@@ -6,6 +6,10 @@ const color = document.getElementById("color");
 
 const colorOptions = Array.from(document.getElementsByClassName("color-option"));
 
+const modeBtn = document.getElementById("mode-btn");
+const destroyBtn = document.getElementById("destroy-btn");
+const eraserBtn = document.getElementById("eraser-btn");
+
 const CANVAS_WIDTH = 800;
 const CANVAS_HEIGHT = 800;
 canvas.width = CANVAS_WIDTH;
@@ -15,6 +19,7 @@ ctx.lineWidth = lineWidth.value;
 ctx.lineCap = "round";
 
 let isPainting = false;
+let isFilling = false;
 
 // 선 그리기
 function onMove(event) {
@@ -31,8 +36,8 @@ function startPaintint() {
 }
 
 function cancelPainting() {
-    ctx.beginPath();
     isPainting = false;
+    ctx.beginPath();
 }
 
 // 선 굵기.
@@ -54,6 +59,37 @@ function onColorClick(event) {
     color.value = colorValue;
 }
 
+// 채우기.
+function onModeClick() {
+    if(isFilling) {
+        isFilling = false;
+        modeBtn.innerText = "Fill";
+    }
+    else {
+        isFilling = true;
+        modeBtn.innerText = "Draw";
+    }
+}
+
+function onCanvasClick() {
+    if(isFilling) {
+        ctx.fillRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+    }
+}
+
+// reset.
+function onDestroyClick() {
+    ctx.fillStyle = "white";
+    ctx.fillRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+}
+
+// 지우기.
+function onEraserClick() {
+    ctx.strokeStyle = "white";
+    isFilling = false;
+    modeBtn.innerText = "Fill";
+}
+
 // 선 그리기.
 canvas.addEventListener("mousemove", onMove);
 canvas.addEventListener("mousedown", startPaintint);
@@ -61,8 +97,12 @@ canvas.addEventListener("mouseup", cancelPainting);
 canvas.addEventListener("mouseleave", cancelPainting);
 // 선 굵기.
 lineWidth.addEventListener("change", onLineWidthChange);
-// 색 설정.
+// 색 설정 및 팔레트.
 color.addEventListener("change", onColorChange);
-// 색 팔레트.
 colorOptions.forEach((color) => color.addEventListener("click", onColorClick));
- 
+// 채우기.
+canvas.addEventListener("click", onCanvasClick);
+modeBtn.addEventListener("click", onModeClick);
+// 리셋 및 지우기.
+destroyBtn.addEventListener("click", onDestroyClick);
+eraserBtn.addEventListener("click", onEraserClick);
